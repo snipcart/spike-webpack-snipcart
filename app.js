89207fb8f37c3531ba46adfd3150fc924c56cde4
Jeff Escalante <escalantejeff@gmail.com>
2017-10-26 15:20:45 -0400

    a little upgrade :cake:

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
 const jsStandards = require('spike-js-standards')
 const pageId = require('spike-page-id')
+const sugarml = require('sugarml')
+const sugarss = require('sugarss')
 const products = require('./assets/data/products/index.js')
+const isProduction = process.env.SPIKE_ENV === 'production'
 
 module.exports = {
   devtool: 'source-map',
-  matchers: {
-    html: '*(**/)*.sgr',
-    css: '*(**/)*.sss'
-  },
+  matchers: { html: '*(**/)*.sgr', css: '*(**/)*.sss' },
   ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
   reshape: htmlStandards({
-    locals: (ctx) => { return { products: products, APIkey: 'YjdiNWIyOTUtZTIyMy00MWMwLTkwNDUtMzI1M2M2NTgxYjE0' } }
+    locals: ctx => {
+      return {
+        products: products,
+        APIkey: 'YjdiNWIyOTUtZTIyMy00MWMwLTkwNDUtMzI1M2M2NTgxYjE0'
+      }
+    },
+    minify: isProduction,
+    parser: sugarml
   }),
-  postcss: cssStandards(),
+  postcss: cssStandards({ minify: isProduction, parser: sugarss }),
   babel: jsStandards()
 }
